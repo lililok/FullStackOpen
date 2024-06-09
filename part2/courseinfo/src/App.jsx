4683afb692b445ad952c776a81137a85274381be
lililok <eshanu.sofia@yandex.ru>
2024-06-09 17:55:02 +0300

    2.2 complete

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,5 +1,5 @@
 const Header = ({ course }) => <h1>{course}</h1>
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+const Total = ({ sum }) => <p>total of {sum} exercises</p>
 const Part = ({ part }) => 
   <p>
     {part.name} {part.exercises}
@@ -9,18 +9,21 @@ const Content = ({ parts }) =>
   <>
     {parts.map(part => (
       <Part key={part.id} part={part} />
-    ))}    
-  </>
+    ))}
+  </>;
 
-const Course = ({ course }) => 
-  <>
-    <Header
-      course={course.name} 
-    />
-    <Content
-      parts={course.parts} 
-    />    
-  </>
+const Course = ({ course }) => {
+  const sum = course.parts.reduce((sum, part) => sum + part.exercises, 0);
+  
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total sum={sum} />
+    </>
+  );
+};
+
 
 const App = () => {
   const course = {
